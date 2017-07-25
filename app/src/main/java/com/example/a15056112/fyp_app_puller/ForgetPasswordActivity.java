6fb1712696001ad0b972e276c7bd58115c2404ae
15056112 <15056112@myrp.edu.sg>
2017-07-25 23:14:46 +0800

    Done

--- app/src/main/java/com/example/a15056112/fyp_app_puller/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/a15056112/fyp_app_puller/ForgetPasswordActivity.java
@@ -2,12 +2,35 @@ package com.example.a15056112.fyp_app_puller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText mEmail;
+    private Button mBtnForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmail = (EditText)findViewById(R.id.forgetEmail);
+        mBtnForgot = (Button)findViewById(R.id.forgetBtn);
+
+        mBtnForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                mAuth.sendPasswordResetEmail(email);
+            }
+        });
     }
 }
